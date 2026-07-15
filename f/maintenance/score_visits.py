fd3633c33c082d469320b499a10a17ffd3d8ca92
carter <carter@jeffspoolspa.com>
2026-07-15 18:49:29 +0000

    [WM] Script 'f/maintenance/score_visits' deployed

--- f/maintenance/score_visits.py
+++ f/maintenance/score_visits.py
@@ -267,7 +267,7 @@ def score_visit(ev, verdicts):
 
 def main(p_start: str = "2026-06-01", p_end: str = "2026-06-30",
          dry_run: bool = False, max_visits: int = 0, skip_llm: bool = False,
-         only_visit_ids: list = None):
+         only_visit_ids: list = ["3a89ddf2-8b5d-4490-a689-31d19cf41a76","e02917ac-0537-4dce-a643-7c3141685dc9","5548064b-2389-4604-94ff-37c448629d2f","43587891-007c-4f4c-a5d4-8dc1582dfd19","e90adeb5-81d6-4433-b9bd-e4105f12a6fd","55dc7062-88b8-428c-b8fa-ce3523499786","c281c302-6d9e-44c3-9d31-8a41bb3ad198","61d802c9-dc5c-48e2-b0b1-4b3a022c33de","5cead687-0fbe-45d0-b3ba-3fd5a21cc96e","4d21f701-95e7-4a3b-b2cc-c7aa887223d5","695954c8-acef-4c5b-bda8-4b66ffabbe68","289a6000-f646-466d-a71d-eb048ea4cdc6","73a059e2-0eaa-4f8d-a43f-4a639930c2bb","a51febe2-e457-446e-837d-fcfce08fb27a","95e9718b-db95-470b-9a77-966493a565c9","50cd10f5-3c22-4de1-b779-21801f1b7758","86678022-316b-4e37-aad9-654973a1b258","cafb950e-eea2-45c3-bd8b-bc0f425a7c78","da4e62c9-de7a-41ac-ac35-7b57a8476f83","4753b39b-7625-4a9a-9ff2-13244c35b88b","fa2b5149-7355-4cbf-82e4-2d2440e824da","107e4f98-eadf-4079-a2d5-f8da4cbf44ad","5b51d6f1-041e-489f-b3d6-d90760c496ac"]):  # ponytail: temp scope to Justin Powell recovery run; revert to None after
     sb = create_client(wmill.get_variable("f/SUPABASE/URL"),
                        wmill.get_variable("f/SUPABASE/SERVICE_ROLE_KEY"))
     akey = wmill.get_variable("f/service_billing/ANTHROPIC_API_KEY")
